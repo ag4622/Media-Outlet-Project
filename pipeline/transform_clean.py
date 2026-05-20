176d2c293eec968dd2f197266863ac97836ce48d
j-tolley <trainee.jessica.tolley@sigmalabs.co.uk>
2026-05-20 12:11:05 +0100

    Filtered for the key information

--- pipeline/transform_clean.py
+++ pipeline/transform_clean.py
@@ -22,13 +22,6 @@ def remove_html_tags(text: str) -> str:
     return clean_text
 
 
-def remove_fields(entry: dict, fields_to_remove: list) -> dict:
-    """Remove specified fields from the entry dictionary."""
-    for field in fields_to_remove:
-        entry.pop(field, None)
-    return entry
-
-
 def format_feed_entries(entry: dict) -> dict:
     """Format the entry data to remove HTML tags like <b>, </b>, <br /> from feed entries."""
     # Remove HTML tags from summary fields
@@ -45,11 +38,30 @@ def format_datetime_fields(entry: dict, datetime_fields: list) -> dict:
     for field in datetime_fields:
         try:
             field_name = field[:field.index('_')]
-            entry[field_name] = datetime.fromtimestamp(mktime(entry[field])).date()
+            entry[field_name] = datetime.fromtimestamp(
+                mktime(entry[field])).date()
         except:
             logging.warning(
                 f"Could not convert field '{field}' to date for entry ID: {entry.get('id', 'N/A')}")
     return entry
+
+
+def format_key_labels(entry: dict) -> dict:
+    """Format key labels to be more human-readable."""
+    key_mapping = {
+        'id': 'trial_id',
+        'summary': 'raw_description',
+        'title': 'title',
+        'updated': 'last_updated',
+        'published': 'published_date',
+        'link': 'source_link'
+    }
+    formatted_entry = {}
+    for key, value in entry.items():
+        new_key = key_mapping.get(key)
+        if new_key:
+            formatted_entry[new_key] = value
+    return formatted_entry
 
 
 def transform(entries: list) -> list[dict]:
@@ -58,12 +70,14 @@ def transform(entries: list) -> list[dict]:
         logging.warning("No entries provided for transformation.")
         return []
     cleaned_entries = []
+    last_ingested = datetime.now().date()
     for entry in entries:
         entry = format_feed_entries(entry)
         entry = format_datetime_fields(
             entry, ['updated_parsed', 'published_parsed'])
-        entry = remove_fields(
-            entry, ['links', 'title_detail', 'summary_detail', 'updated_parsed', 'published_parsed'])
+        entry = format_key_labels(entry)
+        entry = extract_key_information(entry)
+        entry['last_ingested'] = last_ingested
         cleaned_entries.append(entry)
     return cleaned_entries
 
@@ -77,3 +91,4 @@ if __name__ == "__main__":
         if cleaned_entries:
             print(
                 f"First cleaned entry: {cleaned_entries[0]}")
+            print(f"First cleaned entry keys: {cleaned_entries[0].keys()}")
