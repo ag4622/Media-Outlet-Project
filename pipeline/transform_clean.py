84f1264848fdb4c1fb303f09650affb644f2aafe
j-tolley <trainee.jessica.tolley@sigmalabs.co.uk>
2026-05-20 13:34:56 +0100

    Completed inital cleaning of data

--- pipeline/transform_clean.py
+++ pipeline/transform_clean.py
@@ -5,6 +5,44 @@ from time import mktime
 from extract import extract, setup_logging
 
 
+def format_datetime_fields(entry: dict, datetime_fields: list) -> dict:
+    """Format time.struct_time fields to date format (without time)."""
+    for field in datetime_fields:
+        try:
+            field_name = field[:field.index('_')]
+            entry[field_name] = datetime.fromtimestamp(
+                mktime(entry[field])).date()
+        except:
+            logging.warning(
+                f"Could not convert field '{field}' to date for entry ID: {entry.get('trial_id', 'N/A')}")
+    return entry
+
+
+def format_key_labels(entry: dict) -> dict:
+    """Format key labels to be more human-readable."""
+    key_mapping = {
+        'id': 'trial_id',
+        'summary': 'raw_description',
+        'title': 'title',
+        'updated': 'last_updated',
+        'published': 'published_date',
+        'link': 'source_link'
+    }
+    formatted_entry = {}
+    for key, value in entry.items():
+        new_key = key_mapping.get(key)
+        if new_key:
+            formatted_entry[new_key] = value
+    return formatted_entry
+
+
+def extract_key_information(entry: dict) -> dict:
+    """Extract key information from the summary in entry."""
+    summary = entry.get('summary', '')
+
+    print(summary.index('<b>'))
+
+
 def remove_html_tags(text: str) -> str:
     """Remove HTML tags from text using regex pattern."""
     if not isinstance(text, str):
@@ -33,37 +71,6 @@ def format_feed_entries(entry: dict) -> dict:
     return entry
 
 
-def format_datetime_fields(entry: dict, datetime_fields: list) -> dict:
-    """Format time.struct_time fields to date format (without time)."""
-    for field in datetime_fields:
-        try:
-            field_name = field[:field.index('_')]
-            entry[field_name] = datetime.fromtimestamp(
-                mktime(entry[field])).date()
-        except:
-            logging.warning(
-                f"Could not convert field '{field}' to date for entry ID: {entry.get('id', 'N/A')}")
-    return entry
-
-
-def format_key_labels(entry: dict) -> dict:
-    """Format key labels to be more human-readable."""
-    key_mapping = {
-        'id': 'trial_id',
-        'summary': 'raw_description',
-        'title': 'title',
-        'updated': 'last_updated',
-        'published': 'published_date',
-        'link': 'source_link'
-    }
-    formatted_entry = {}
-    for key, value in entry.items():
-        new_key = key_mapping.get(key)
-        if new_key:
-            formatted_entry[new_key] = value
-    return formatted_entry
-
-
 def transform(entries: list) -> list[dict]:
     """Transforms and cleans feed entries by removing HTML tags."""
     if not entries:
@@ -72,23 +79,20 @@ def transform(entries: list) -> list[dict]:
     cleaned_entries = []
     last_ingested = datetime.now().date()
     for entry in entries:
-        entry = format_feed_entries(entry)
         entry = format_datetime_fields(
             entry, ['updated_parsed', 'published_parsed'])
         entry = format_key_labels(entry)
-        entry = extract_key_information(entry)
         entry['last_ingested'] = last_ingested
         cleaned_entries.append(entry)
+    logging.info(f"Transformed {len(cleaned_entries)} entries")
     return cleaned_entries
 
 
 if __name__ == "__main__":
     setup_logging()
     entries = extract()
-    if entries:
-        cleaned_entries = transform(entries)
-        print(f"Transformed {len(cleaned_entries)} entries")
-        if cleaned_entries:
-            print(
-                f"First cleaned entry: {cleaned_entries[0]}")
-            print(f"First cleaned entry keys: {cleaned_entries[0].keys()}")
+    cleaned_entries = transform(entries)
+    print(cleaned_entries[0])
+
+    print(entries[0].get('summary', 'No summary available'))
+    extract_key_information(entries[0])
